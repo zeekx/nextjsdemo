951c83737153f68fc391cd734204fb5bd6635e23
Zeekx <yuprxk@163.com>
2025-01-16 16:35:12 +0800

    Prepare to Github

---
+++ nextjs-dashboard/app/ui/fonts.tsx
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
